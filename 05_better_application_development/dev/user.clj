0b29b77db8e3915a876bb0c6bc74a9aed1a670da
Daniel King <daniel.oliver.king@gmail.com>
2019-06-05 09:05:31 -0700

    Add development workflow functions to user namespace

--- 05_better_application_development/dev/user.clj
+++ 05_better_application_development/dev/user.clj
@@ -1,1 +1,26 @@
-(ns user)
+(ns user
+  (:require [integrant.core :as ig]
+            [clojure.tools.namespace.repl :as ctnr]
+            [com.shortify.system :as sys]
+            [com.shortify.db.migration :as migration]
+            [com.shortify.db.seed :as seed]))
+
+(def system nil)
+
+(defn start
+  []
+  (println "Starting system...")
+  (let [new-system (ig/init sys/config)]
+    (alter-var-root (var system) (constantly new-system))))
+
+(defn stop
+  []
+  (when system
+    (println "Stopping system...")
+    (ig/halt! system)
+    (alter-var-root (var system) (constantly nil))))
+
+(defn reset
+  []
+  (stop)
+  (ctnr/refresh :after 'user/start))
