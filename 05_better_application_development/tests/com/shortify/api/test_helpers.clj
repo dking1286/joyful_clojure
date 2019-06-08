7e30a4603787453c33cb6a893d353440a4ebe473
Daniel King <daniel.oliver.king@gmail.com>
2019-06-08 09:31:18 -0700

    Add documentation to test helper functions.

--- 05_better_application_development/tests/com/shortify/api/test_helpers.clj
+++ 05_better_application_development/tests/com/shortify/api/test_helpers.clj
@@ -7,16 +7,23 @@
             [com.shortify.api.system :as sys]))
 
 (defn- to-reader
+  "Converts a Clojure data structure to a Reader containing a JSON
+  representation of the data structure."
   [body]
   (StringReader. (json/write-str body)))
 
 (defn- serialize
+  "Converts a Clojure data structure in the body of a request into a reader
+  containing a JSON representation of the data structure. This is necessary
+  because the ring-json middleware expects the request body to be a Reader."
   [req]
   (-> req
       (assoc-in [:headers "content-type"] "application/json")
       (update :body to-reader)))
 
 (defn- parse
+  "Converts a JSON string in the body of a response into an equivalent
+  Clojure data structure."
   [res]
   (try
     (update res :body #(json/read-str % :key-fn keyword))
@@ -24,6 +31,10 @@
       res)))
 
 (defn request
+  "Simulates sending a request to the application for testing.
+
+  The request body should be a plain Clojure data structure. A response map
+  with a plain Clojure data structure in the body is returned."
   [app req]
   (parse (app (serialize req))))
 
@@ -39,13 +50,21 @@
       ;; b/c we can call the handler directly.
       (dissoc :server)))
 
+(defn- reset-database!
+  "Resets the database to a known state for testing."
+  [db]
+  (with-out-str
+    (migration/migrate-down! db)
+    (migration/migrate-up! db))
+  (seed/insert-all-seeds! db))
+
 (defn with-test-system
+  "Test fixture to set up and tear down the test system.
+
+  The test is run with the test system bound to the *system* dynamic var"
   [run-tests]
   (let [{:keys [db] :as system} (ig/init test-config)]
-    (with-out-str
-      (migration/migrate-down! db)
-      (migration/migrate-up! db))
-    (seed/insert-all-seeds! db)
+    (reset-database! db)
     (binding [*system* system]
       (run-tests))
     (ig/halt! system)))
